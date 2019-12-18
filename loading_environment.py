41aa5728b5c13b262c8f6805a02d5d9cec16a43e
Ankush <ankush.mitra@students.iiit.ac.in>
2019-12-18 16:57:09 +0530

    important checkpoint

---
+++ loading_environment.py
@@ -0,0 +1,13 @@
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+REDIS_HOST=str(os.environ.get("REDIS_HOST"))
+REDIS_PORT=int(os.environ.get("REDIS_PORT"))
+MQTT_HOST=str(os.environ.get("MQTT_HOST"))
+MQTT_PORT=int(os.environ.get("MQTT_PORT"))
+WSS_ADD=str(os.environ.get("WSS_ADD"))
+TILE_HOST=str(os.environ.get("TILE_HOST"))
+TILE_PORT=int(os.environ.get("TILE_PORT"))
